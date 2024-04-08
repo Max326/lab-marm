fef2fb59c7a131995cc5654ff7261e952d3a018f
Max326 <maxconnor326@gmail.com>
2024-04-08 23:05:26 +0200

    reverted my changes

--- stm32f411e_disco/e0_led_blink/src/blink.cpp
+++ stm32f411e_disco/e0_led_blink/src/blink.cpp
@@ -10,15 +10,15 @@ namespace {
 }
 
 namespace app {
-    // void test_thread(void*) {
-    //     for(int i=0;;++i) {
-    //         isix::wait_ms(500);
-    //         if(i%2==0) {
-    //             dbprintf("Loop %i",i>>1);
-    //         }
-    //         periph::gpio::set(led_0, i%2);
-    //     }
-    // }
+    void test_thread(void*) {
+        for(int i=0;;++i) {
+            isix::wait_ms(500);
+            if(i%2==0) {
+                dbprintf("Loop %i",i>>1);
+            }
+            periph::gpio::set(led_0, i%2);
+        }
+    }
 
     void print_array(int tab[], int tabLength){
     for(int i = 0; i < tabLength; ++i) {
@@ -61,59 +61,39 @@ namespace app {
         print_array(tab, tabLength);
     }
 
-    void test_thread(void*) {
-        for(int i = 0; i < 10; ++i) { // Przykład: wykonaj sortowanie 10 razy
-            test_sort_function();
-            isix::wait_ms(500); // Odczekaj sekundę między testami
-        }
-    //     constexpr int tabLength = 10;
-    //     int tab[tabLength];
-    //     // Inicjalizacja generatora liczb losowych
-    //     srand(time(nullptr));
-    //     for(int n = 0; n < 50; ++n) { // Generuj i sortuj 50 razy
-    //         // Generowanie nowej tablicy z losowymi danymi
-    //         for(int i = 0; i < tabLength; ++i) {
-    //             tab[i] = rand() % 100; // Liczby z zakresu 0-99
-    //         }
-    //         dbprintf("Tablica przed sortowaniem:");
-    //         printArray(tab, tabLength);
-    //         // Sortowanie tablicy
-    //         sort(tab, tabLength);
-    //         dbprintf("Tablica po sortowaniu:");
-    //         printArray(tab, tabLength);
-    //         // Czekaj 2 sekundy przed kolejnym cyklem
-    //         isix::wait_ms(2000);
-    //     }
-    // }    
-}
+    // void test_thread(void*) {
+    //     for(int i = 0; i < 10; ++i) { 
+    //         test_sort_function();
+    //     }   
+    // }
 }
 
 
 auto main() -> int
 {
-	// static isix::semaphore m_ulock_sem { 1, 1 };
-    // isix::wait_ms( 500 );
-	// dblog_init_locked(
-	// 	[](int ch, void*) {
-	// 		return periph::drivers::uart_early::putc(ch);
-	// 	},
-	// 	nullptr,
-	// 	[]() {
-	// 		m_ulock_sem.wait(ISIX_TIME_INFINITE);
-	// 	},
-	// 	[]() {
-	// 		m_ulock_sem.signal();
-	// 	},
-	// 	periph::drivers::uart_early::open,
-	// 	"serial0", 115200
-	// );
+	static isix::semaphore m_ulock_sem { 1, 1 };
+    isix::wait_ms( 500 );
+	dblog_init_locked(
+		[](int ch, void*) {
+			return periph::drivers::uart_early::putc(ch);
+		},
+		nullptr,
+		[]() {
+			m_ulock_sem.wait(ISIX_TIME_INFINITE);
+		},
+		[]() {
+			m_ulock_sem.signal();
+		},
+		periph::drivers::uart_early::open,
+		"serial0", 115200
+	);
     // Configure PD13 pin LED as an output
-    // periph::gpio::setup( led_0,
-    //     periph::gpio::mode::out{
-    //         periph::gpio::outtype::pushpull,
-    //         periph::gpio::speed::low
-    //     }
-    // );
+    periph::gpio::setup( led_0,
+        periph::gpio::mode::out{
+            periph::gpio::outtype::pushpull,
+            periph::gpio::speed::low
+        }
+    );
 	isix::task_create( app::test_thread, nullptr, 1536, isix::get_min_priority() );
     dbprintf("<<<< Hello STM32F411E-DISCO board >>>>");
 	isix::start_scheduler();
